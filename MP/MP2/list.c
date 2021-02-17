37887f32558255492be86a5158b129a8f8f35242
Aaron Joseph Bruner <ajbrune@g.clemson.edu>
2021-02-17 00:16:58 -0500

    MP2 Progress

--- MP/MP2/list.c
+++ MP/MP2/list.c
@@ -1,14 +1,17 @@
-/* list.c                  <<-- A template to update and 
- * Prof. Calhoun           <<-- change
- * jonccal
+/* list.c
+ * Aaron Bruner
+ * ajbrune
  * ECE 2230 Spring 2021
  * MP2
  *
- * Propose: A template for list.c. You will make many changes.
+ * Propose: The purpose of MP 2 is to build an understanding of two-way linked lists. We based the idea of this
+ *          machine problem mostly on MP 1, which used a dynamic array. Now, we're referencing our new list
+ *          which should be more reusable for future assignments.
  *
- * Assumptions: Many details are incomplete.  
+ * Assumptions: We must assume that the user operating the code understands how to properly run the program. Also
+ *              we must assume that the user only provides valid input which will not break the lab2.c file.
  *
- * Bugs: Many details have not been implemented.
+ * Bugs: None that I know of. (Other than inputting extraneously massive numbers which cause buffer overflow)
  *
  * The interface definition for the two-way linked list ADT is based on one 
  * defined by OPNET, Inc. http://www.opnet.com/
@@ -48,7 +51,6 @@ void list_debug_validate(list_t *L);
  */
 data_t * list_access(list_t *list_ptr, int pos_index)
 {
-    int count;
     list_node_t *L;
  
     assert(list_ptr != NULL);
@@ -56,15 +58,25 @@ data_t * list_access(list_t *list_ptr, int pos_index)
     /* debugging function to verify that the structure of the list is valid */
     list_debug_validate(list_ptr);
 
-    /* TODO: handle four special cases.
-     *   1.  The list is empty
-     *   2.  Asking for the head 
-     *   3.  Asking for the tail
-     *   4.  Asking for invalid pos
-     */
-
-    /* we now know pos_index is for an interal element */
-    /* TODO: loop through the list until find correct position index */
+    if ( list_ptr->current_list_size == 0 || list_ptr->head == NULL || list_ptr == NULL) { // List is empty
+        return NULL;
+    }
+    else if ( pos_index == 0 || pos_index == LISTPOS_HEAD) { // Head
+        L = list_ptr->head;
+    }
+    else if ( pos_index == (list_ptr->current_list_size)-1 || pos_index == LISTPOS_TAIL) { // Tail
+        L = list_ptr->tail;
+    }
+    else if ( pos_index >= list_ptr->current_list_size || (pos_index < 0 && pos_index != LISTPOS_HEAD
+                                                                                    && pos_index != LISTPOS_TAIL) ) {
+        return NULL;
+    }
+    else { // we need to find the index value
+        L = list_ptr->head;
+        for (int i = 0; i < pos_index; i++) {
+            L = L->next;
+        }
+    }
 
     assert(L != NULL);
     return L->data_ptr;
@@ -124,10 +136,24 @@ data_t * list_elem_find(list_t *list_ptr, data_t *elem_ptr, int *pos_index)
 {
     list_debug_validate(list_ptr);
 
-    /*TODO: */
-    /* fix the return value */
     *pos_index = -1;
-    return NULL;
+    list_node_t *list = list_ptr->head; // initialize our list to the start of the data
+    data_t *returnValue = NULL;
+
+    if (list_access(list_ptr, *pos_index) == NULL) return NULL; // check for invalid index
+
+    for (int i = 0; i < list_ptr->current_list_size; i++) {
+        if ( list_ptr->comp_proc(list->data_ptr, elem_ptr) == 0) { // if the i value we're at matches elem_ptr then
+            *pos_index = i;                                        // pos_index = i and break out of the loop
+            break;
+        } else {
+            list = list->next; // If it isn't a match then go to the next one
+        }
+    }
+
+    returnValue = list->data_ptr; // Just setting the return value to the data pointer
+
+    return returnValue;
 }
 
 /* Deallocates the contents of the specified list, releasing associated memory
@@ -141,8 +167,18 @@ void list_destruct(list_t *list_ptr)
     /* the first line must validate the list */
     list_debug_validate(list_ptr);
 
-    /*TODO*/
-    // Your code starts here
+    if ( list_ptr->current_list_size == 0 ) free(list_ptr); // List is empty
+
+    else { // If the list is not empty then free everything from the last element back to the first
+        for (int i = (list_ptr->current_list_size)-1; i >= 0; i--) {
+            list_node_t *list = list_ptr->tail;
+            free(list_ptr->tail->data_ptr);         // Free the data at the tail pointer
+            list_ptr->tail = list_ptr->tail->prev;  // Shift the pointer to the previous element
+            free(list);
+        }
+        free(list_ptr);
+    }
+
 }
 
 /* Inserts the specified data element into the specified list at the specified
@@ -171,7 +207,41 @@ void list_destruct(list_t *list_ptr)
 void list_insert(list_t *list_ptr, data_t *elem_ptr, int pos_index)
 {
     assert(list_ptr != NULL);
-    /* TODO: STUDENT*/
+
+    list_node_t *list = (list_node_t *)malloc(sizeof(list_node_t)); if (list == NULL) exit(1);
+    list->prev = NULL;
+    list->next = NULL;
+    list->data_ptr = elem_ptr;
+
+    list_node_t *start = list_ptr->head;
+    list_node_t *end;
+
+    if (list_ptr->current_list_size == 0 || (list_ptr->head == NULL && list_ptr->tail == NULL)) { // Empty List
+        list_ptr->head = list;
+        list_ptr->tail = list;
+    }
+    else if (pos_index == 0 || pos_index == LISTPOS_HEAD) { // HEAD
+        list->next = list_ptr->head;
+        list_ptr->head->prev = list;
+        list_ptr->head = list;
+    }
+    else if (pos_index >= list_ptr->current_list_size || pos_index == LISTPOS_TAIL) { // TAIL
+        list->prev = list_ptr->tail;
+        list_ptr->tail->next = list;
+        list_ptr->tail = list;
+    }
+    else {
+        for (int i = 0; i < pos_index; i++) start = start->next; // Get to the pos_index position
+
+        end = start->prev;
+        list->prev = end;
+        end->next = list;
+        list->next = start;
+        start->prev = list;
+    }
+
+    list_ptr->current_list_size += 1;
+
     /* the last two lines of this function must be the following */
     if (list_ptr->list_sorted_state != LIST_UNSORTED)
         list_ptr->list_sorted_state = LIST_UNSORTED;
@@ -201,7 +271,7 @@ void list_insert(list_t *list_ptr, data_t *elem_ptr, int pos_index)
  *   -1: B should be closer to the list head than A
  *    0: A and B are equal in rank
  * This definition results in the list being in ascending order.  To insert
- * in decending order, change the sign of the value that is returned.
+ * in descending order, change the sign of the value that is returned.
  *
  * Note: if the element to be inserted is equal in rank to an element already
  * in the list, the newly inserted element will be placed after all the
@@ -212,8 +282,26 @@ void list_insert_sorted(list_t *list_ptr, data_t *elem_ptr)
     assert(list_ptr != NULL);
     assert(list_ptr->list_sorted_state != LIST_UNSORTED);
 
-    /* insert your code here */
-    /* TODO: STUDENT */
+    int position = 0, current_state = list_ptr->list_sorted_state;
+    list_node_t *list = list_ptr->head;
+
+    if (list_ptr->list_sorted_state == LIST_SORTED_ASCENDING) { // Ascending
+        while (list != NULL && list_ptr->comp_proc(list->data_ptr, elem_ptr) != -1){
+            position += 1;
+            list = list->next;
+        }
+    }
+    else if (list_ptr->list_sorted_state == LIST_SORTED_DESCENDING) { // Descending
+        while (list != NULL && list_ptr->comp_proc(list->data_ptr, elem_ptr) != 1){
+            position += 1;
+            list = list->next;
+        }
+    }
+
+    list_insert(list_ptr, elem_ptr, position);
+
+    list_ptr->list_sorted_state = current_state;
+
     /* the last line of this function must be the following */
     list_debug_validate(list_ptr);
 }
@@ -233,24 +321,89 @@ void list_insert_sorted(list_t *list_ptr, data_t *elem_ptr)
  */
 data_t * list_remove(list_t *list_ptr, int pos_index)
 {
+    if (list_access(list_ptr, pos_index) == NULL) return NULL; // List doesn't exist
 
-    /*TODO: STUDENT*/
+    // We're going to use temp and temp_data as a copy of our removed index value
+    list_node_t *temp;
+    data_t *temp_data;
+
+    list_node_t *next, *prev;
+
+    if ((pos_index == 0 || pos_index == LISTPOS_HEAD || pos_index == LISTPOS_TAIL) // list of size 1
+                                                                                && list_ptr->current_list_size == 1) {
+        temp = list_ptr->head; // Copy the head into temp & data into temp_data
+        temp_data = temp->data_ptr;
+
+        list_ptr->head = NULL; // Remove the data at head and tail
+        list_ptr->tail = NULL;
+    }
+    else if (pos_index == 0 || pos_index == LISTPOS_HEAD) { // Remove Head
+
+        temp = list_ptr->head;
+        temp_data = temp->data_ptr;
+
+        list_ptr->head = list_ptr->head->next;
+        list_ptr->head->prev = NULL;
+    }
+    else if (pos_index == (list_ptr->current_list_size)-1 || pos_index == LISTPOS_TAIL) { // Remove Tail
+
+        temp = list_ptr->tail;
+        temp_data = temp->data_ptr;
+
+        list_ptr->tail = list_ptr->tail->prev;
+        list_ptr->tail->next = NULL;
+    }
+    else { // Remove pos_index node
+        if (list_access(list_ptr, pos_index) == NULL) return NULL;
+
+        temp = list_ptr->head;
+        for ( int i = 0; i < pos_index; i++) temp = temp->next; // Get to the pos_index value
+        temp_data = temp->data_ptr;
+
+        next = temp->next;
+        prev = temp->prev;
+
+        prev->next = next;
+        next->prev = prev;
+
+    }
+    list_ptr->current_list_size -= 1; // Decrement the list size
+    free(temp);
+
     list_debug_validate(list_ptr);
 
-    /* fix the return value */
-    return NULL;
+    return temp_data;
 }
 
 /* Reverse the order of the elements in the list.  Also change the 
  * list_sorted_state flag.  This function can only be called on a list
  * that is sorted.
  *
- * list_ptr: pointer to list-of-interest.  
+ * list_ptr: pointer to list-of-interest.
  */
 void list_reverse(list_t *list_ptr)
 {
     assert(list_order(list_ptr) != 0);
-    /* TODO: STUDENT */
+
+    //if (list_ptr == NULL || list_ptr->current_list_size == 1 || (list_ptr->head == NULL && list_ptr->tail == NULL));
+
+    if (list_ptr->head != list_ptr->tail && list_ptr->current_list_size > 1) {
+
+        list_node_t *prevNode, *curNode = list_ptr->head;
+        list_ptr->tail = curNode;
+
+        while ( curNode != NULL ) {
+            prevNode = curNode->prev;
+            curNode->prev = curNode->next;
+            curNode->next = prevNode;
+            curNode = curNode->prev;
+        }
+        list_ptr->head = prevNode->prev;
+    }
+
+    // Inverts whatever the sorted state is
+    list_ptr->list_sorted_state == LIST_SORTED_ASCENDING ? (list_ptr->list_sorted_state = LIST_SORTED_DESCENDING) :
+                                                            (list_ptr->list_sorted_state = LIST_SORTED_ASCENDING);
 
     // after the list is reversed verify it is valid.
     list_debug_validate(list_ptr);
